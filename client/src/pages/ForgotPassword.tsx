fea19960cadd280015765690fb7f276a66fe9402
Revisao Segura <segurarevisao@gmail.com>
2026-01-10 22:46:58 -0300

    novo

--- client/src/pages/ForgotPassword.tsx
+++ client/src/pages/ForgotPassword.tsx
@@ -1,5 +1,4 @@
-import { useState } from  npm install react
- npm install --save-dev @types/react;
+import { useState } from "react";
 import { Link } from "wouter";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
